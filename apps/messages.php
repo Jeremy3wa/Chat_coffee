42a5377ba37972bb3a22bb1a670382e8a51e7b52
Jeremy3wa <Jeremy.6796@hotmail.fr>
2017-02-17 15:30:06 +0100

    f

--- apps/messages.php
+++ apps/messages.php
@@ -1,8 +1,6 @@
 <?php
 $res = mysqli_query($db, "SELECT messages.* , users.login FROM  `messages` , users WHERE users.id = messages.id_author");
-while ($message = mysqli_fetch_assoc($res))
-{
+
 require('views/messages.phtml');
 
-}
 ?>
