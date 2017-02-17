8fc6fb1ef686885ed7ef07601d3e7d1508139ff2
Jeremy3wa <Jeremy.6796@hotmail.fr>
2017-02-17 16:14:20 +0100

    f

--- apps/messages.php
+++ apps/messages.php
@@ -1,5 +1,5 @@
 <?php
-$res = mysqli_query($db, "SELECT messages.* , users.login FROM  `messages` , users WHERE users.id = messages.id_author");
+$res = mysqli_query($db, "SELECT messages.* , users.login FROM  `messages` , users WHERE users.id = messages.id_author ORDER BY messages.date DESC");
 
 require('views/messages.phtml');
 
