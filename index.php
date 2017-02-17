a38605260cd06a2691a6b1cf962b5cdbac5aaba2
nellygrr <neeeeelly@gmail.com>
2017-02-17 11:55:38 +0100

    index du chat

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$errors = [];
+$db = mysqli_connect("192.168.1.52", "tchat", "tchat", "tchat");
+session_start();// http://php.net/manual/fr/function.session-start.php
+
+$access = ["login", "register", "messages", "create_message", ];
+$page = "messages";
+if (isset($_GET['page']) && in_array($_GET['page'], $access)) // http://php.net/manual/fr/function.in-array.php
+{
+    $page = $_GET['page'];
+}
+
+require('apps/traitement_users.php');
+require('apps/traitement_messages.php');
+require('apps/skel.php');
+?>
+
