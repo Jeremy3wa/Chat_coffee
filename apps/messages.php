7bdabebd7a4467290ca792106227011ea333bb77
Jeremy3wa <Jeremy.6796@hotmail.fr>
2017-02-17 13:47:23 +0100

    fichiers php

--- apps/messages.php
+++ apps/messages.php
@@ -1,1 +1,8 @@
-<?php require('views/messages.phtml'); ?>
+<?php
+$res = mysqli_query($db, "SELECT messages.* , users.login FROM  `messages` , users WHERE users.id = messages.id_auteur AND messages.id_article=".$message['id']);
+while ($messages = mysqli_fetch_assoc($res))
+{
+require('views/messages.phtml');
+
+}
+?>
---
+++ apps/traitement_messages.php
@@ -0,0 +1,45 @@
+<?php
+var_dump($_POST);
+?>
+<?php
+// Etape 0
+// titre, content, auteur
+// Etape 1
+if (isset($_POST['content'])
+{
+	// Etape 2
+	$content = $_POST['content'];// 4095
+	if (strlen($content) > 4095)
+	{
+		$errors[] = "Contenu trop long (> 4095)";
+	}
+	else if (strlen($content) < 2)
+	{
+		$errors[] = "Contenu trop court (< 2)";
+	}
+	// Etape 3
+	if (count($errors) == 0)
+	{
+		$content = mysqli_real_escape_string($db, $content);
+		$res = mysqli_query($db, "INSERT INTO comments (content, id_auteur, id_article) VALUES('".$content."', '".$_SESSION['id']."')");
+		if ($res)
+		{
+		/*
+			INSERT INTO articles (titre, content, auteur) VALUES('titre', 'contenu', 'auteur')
+			INSERT INTO articles (titre, content, auteur) VALUES('$titre', '$content', '$auteur')
+			"INSERT INTO articles (titre, content, auteur) VALUES('$titre', '$content', '$auteur')"
+			"INSERT INTO articles (titre, content, auteur) VALUES('.$titre.', '.$content.', '.$auteur.')"
+			"INSERT INTO articles (titre, content, auteur) VALUES('".$titre."', '".$content."', '".$auteur."')"
+			*/
+			// "INSERT INTO " => '".$toto."'
+			// Etape 4
+			header('Location: index.php');
+			exit;
+		}
+		else
+		{
+			$errors[] = "Erreur interne";
+		}
+	}
+}
+?>
