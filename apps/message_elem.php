3b07c2daeb9340e7f08d85f92dd7fd336551a8ef
Jeremy3wa <Jeremy.6796@hotmail.fr>
2017-02-20 10:41:17 +0100

    Ajax

--- apps/message_elem.php
+++ apps/message_elem.php
@@ -1,4 +1,6 @@
 <?php
+$res = mysqli_query($db, "SELECT messages.*, users.login, users.avatar FROM  `messages` , users WHERE users.id = messages.id_author ORDER BY messages.date DESC");
+
 while ($message = mysqli_fetch_assoc($res))
 {
 	require('views/message_elem.phtml');
--- apps/messages.php
+++ apps/messages.php
@@ -1,6 +1,5 @@
 <?php
 
-$res = mysqli_query($db, "SELECT messages.*, users.login, users.avatar FROM  `messages` , users WHERE users.id = messages.id_author ORDER BY messages.date DESC");
 
 require('views/messages.phtml');
 
